2d671f4c9d47c1b7086fa6936f2497eee0bf0a4d
Imilla Dev <lupemaydanda@gmail.com>
2020-08-04 10:26:28 -0400

    feat: Sign in function with email and password

--- src/services/firebaseAuth.js
+++ src/services/firebaseAuth.js
@@ -1,9 +1,11 @@
 const firebase = window.firebase;
 
-firebase.auth().createUserWithEmailAndPassword("hola@pandemia.com", "pancito").catch(function(error) {
+/* firebase.auth().createUserWithEmailAndPassword("hola@pandemia.com", "pancito").catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
     // ...
   });
+ */
 
+  export const signIn = (email, password) => firebase.auth().signInWithEmailAndPassword(email, password);

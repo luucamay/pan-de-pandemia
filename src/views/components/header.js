9459e349a6a7db1bbb36c11ed418a896be48a61d
Imilla Dev <lupemaydanda@gmail.com>
2020-08-06 00:05:17 -0400

    feat: Add header component

--- src/views/components/header.js
+++ src/views/components/header.js
@@ -1,10 +1,12 @@
-const Header = () => {
-    const view = `
-    <header>
-        <h1>PanDemia</h1>
-    </header>
-    `;
-    return view
+const Header = {
+    render: async() => {
+        const view = `
+        <header>
+            <h1>PanDemia</h1>
+        </header>
+        `;
+        return view
+    }
 }
 
 export default Header;

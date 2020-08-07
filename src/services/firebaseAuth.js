ce8f501a7f8dcc7dafb09ad31a762e6f48540538
Imilla Dev <lupemaydanda@gmail.com>
2020-08-06 22:17:22 -0400

    feat: Create signIn function

--- src/services/firebaseAuth.js
+++ src/services/firebaseAuth.js
@@ -1,9 +1,12 @@
 const firebase = window.firebase;
 
-firebase.auth().createUserWithEmailAndPassword("hola@pandemia.com", "pancito").catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-  });
+// Sign in User
+export const signIn = (email, password) => {
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then(console.log)
+    .catch(console.log);
+}
 
+export default signIn;

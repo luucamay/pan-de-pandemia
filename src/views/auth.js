b93e7ce6a7539432e840a2e515952018ae0f70c2
Imilla Dev <lupemaydanda@gmail.com>
2020-08-06 22:23:57 -0400

    feat: Add event to the sign in button

--- src/views/auth.js
+++ src/views/auth.js
@@ -1,12 +1,22 @@
 import Input from './components/input.js';
 import Button from './components/button.js';
+import { signIn } from '../services/firebaseAuth.js';
 
 const Auth = {
     render: async () => {
+        const view = document.createElement('div');
         const email = await Input.render('Email', 'text');
         const password = await Input.render('Contrasenia', 'password');
-        const button = await Button.render('Enviar');
-        const view =  email + password + button;
+        const button = await Button.render('Enviar', 'signInBtn');
+        view.innerHTML = email + password + button;
+
+        // Login with access
+        view.querySelector('#signInBtn').addEventListener('click', () => {
+            const email = document.getElementById('email').value;
+            const password = document.getElementById('password').value;
+            signIn(email, password);
+        });
+
         return view
     }
 }
